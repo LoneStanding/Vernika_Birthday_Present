11b71159231b77d9b0c915de4f5e1da0f0a4684a
LoneStanding <param.akhouri@gmail.com>
2024-01-22 04:09:49 +0530

    Implemented collage grid and added pics

---
+++ index.js
@@ -0,0 +1,34 @@
+const wrapper = document.getElementById('size_grid')
+
+columns = Math.floor(document.body.clientWidth / 50);
+rows = Math.floor(document.body.clientHeight / 50);
+
+const createTile = index => {
+    const tile = document.createElement('div');
+    tile.classList.add('tile');
+    return tile;
+}
+
+const createTiles = quantity => {
+    Array.from(Array(quantity)).map((tile, index) => {
+        wrapper.appendChild(createTile(index));
+    })
+}
+
+const createGrid = () => {
+    const tile = document.querySelectorAll('.tile');
+
+    tile.forEach(tile => {
+        tile.remove();
+    });
+
+    columns = Math.floor(document.body.clientWidth / 50);
+    rows = Math.floor(document.body.clientHeight / 50);
+
+    wrapper.style.setProperty("--columns", columns);
+    wrapper.style.setProperty("--rows", rows);
+    
+    createTiles(columns * rows);
+}
+createGrid();
+window.onresize = () => createGrid();
